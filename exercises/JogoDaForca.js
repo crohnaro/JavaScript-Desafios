c177d025610cd3fd118ccab398b26adf3deaae29
crohnaro <vinicioscararine95@gmail.com>
2024-05-28 21:13:16 -0300

    feat: :art: adding function signatures

--- exercises/JogoDaForca.js
+++ exercises/JogoDaForca.js
@@ -1,2 +1,21 @@
 /* Jogo da Forca Simplificado: Implemente um jogo da forca onde o usuário deve adivinhar uma palavra secreta. A cada tentativa errada, informe ao usuário quantas tentativas restam. O jogo deve terminar quando o usuário acertar a palavra ou esgotar as tentativas. */
 
+class Hangman {
+  constructor(word, maxAttempts) {
+    
+  }
+
+  guess(letter) {
+    
+  }
+
+  getWordProgress() {
+    
+  }
+
+  isGameOver() {
+   
+  }
+}
+
+module.exports = Hangman;
--- exercises/SenhasAleatorias.js
+++ exercises/SenhasAleatorias.js
@@ -1,1 +1,22 @@
 /*Gerador de Senhas Aleatórias: Crie um programa que gere senhas aleatórias seguras com base nos critérios definidos pelo usuário (comprimento da senha, inclusão de letras maiúsculas, minúsculas, números e símbolos).*/
+
+function generatePassword(length, options) {
+  const chars = {
+    
+  };
+
+  let availableChars = "";
+  if (options.lower) 
+  if (options.upper) 
+  if (options.number) 
+  if (options.symbol);
+
+  let password = "";
+  for (let i = 0; i < length; i++) {
+    
+  }
+
+  return password;
+}
+
+module.exports = generatePassword;
