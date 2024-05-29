f4a45c95ac3969f8a2b71605cff3fa8fa8af578f
ViniDev2 <canaritorevoltado@gmail.com>
2024-05-28 21:25:48 -0300

    Update JogoDaForca.js

--- exercises/JogoDaForca.js
+++ exercises/JogoDaForca.js
@@ -2,19 +2,25 @@
 
 class Hangman {
   constructor(word, maxAttempts) {
-    
+    this.word = word;
+    this.maxAttempts = maxAttempts;
+    this.attempts = 0;
+    this.guessedLetters = [];
   }
 
   guess(letter) {
-    
+    if (this.attempts >= this.maxAttempts) return 'Game Over';
+    this.guessedLetters.push(letter);
+    if (!this.word.includes(letter)) this.attempts++;
+    return this.getWordProgress();
   }
 
   getWordProgress() {
-    
+    return this.word.split('').map(letter => (this.guessedLetters.includes(letter) ? letter : '_')).join('');
   }
 
   isGameOver() {
-   
+    return this.attempts >= this.maxAttempts || this.word === this.getWordProgress();
   }
 }
 
