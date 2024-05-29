1d646fa966cc3edd7879282d0accace1e80cdf17
ViniDev2 <canaritorevoltado@gmail.com>
2024-05-28 21:26:07 -0300

    Update SenhasAleatorias.js

--- exercises/SenhasAleatorias.js
+++ exercises/SenhasAleatorias.js
@@ -2,20 +2,23 @@
 
 function generatePassword(length, options) {
   const chars = {
-    
+    lower: 'abcdefghijklmnopqrstuvwxyz',
+    upper: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+    number: '0123456789',
+    symbol: '!@#$%^&*()_+[]{}|;:,.<>?'
   };
-
-  let availableChars = "";
-  if (options.lower) 
-  if (options.upper) 
-  if (options.number) 
-  if (options.symbol);
-
-  let password = "";
+  
+  let availableChars = '';
+  if (options.lower) availableChars += chars.lower;
+  if (options.upper) availableChars += chars.upper;
+  if (options.number) availableChars += chars.number;
+  if (options.symbol) availableChars += chars.symbol;
+  
+  let password = '';
   for (let i = 0; i < length; i++) {
-    
+    password += availableChars.charAt(Math.floor(Math.random() * availableChars.length));
   }
-
+  
   return password;
 }
 
