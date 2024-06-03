63e4f8884945630f99b1a4946403e1576f31f4e7
Vale-JP <dmoreiravale@gmail.com>
2024-06-03 15:51:24 -0300

    Solução dos exercícios

--- exercises/JogoDaForca.js
+++ exercises/JogoDaForca.js
@@ -2,19 +2,38 @@
 
 class Hangman {
   constructor(word, maxAttempts) {
-    
+    this.word = word.toLowerCase();
+    this.maxAttempts = maxAttempts;
+    this.attemptsLeft = maxAttempts;
+    this.guessedLetters = new Set();
   }
 
   guess(letter) {
-    
+    letter = letter.toLowerCase();
+    if (!this.isGameOver() && !this.guessedLetters.has(letter)) {
+      this.guessedLetters.add(letter);
+      if (!this.word.includes(letter)) {
+        this.attemptsLeft--; 
+      }
+    }
   }
 
   getWordProgress() {
-    
+    return this.word.split('').map(letter => {
+      if(this.guessedLetters.has(letter)) {
+        return letter;
+      } else {
+        return '_';
+      }
+    }).join('');
   }
 
   isGameOver() {
-   
+   return this.wordGuessed() || this.attemptsLeft === 0;
+  }
+
+  wordGuessed() {
+    return this.word.split('').every(letter => this.guessedLetters.has(letter));
   }
 }
 
