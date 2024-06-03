f1cc51b4f1d3328d327a10a3b590f4cb229c7eb6
Daniel Silva Passos <961377@senacminas.edu.br>
2024-06-03 16:44:00 -0300

    Soluções dos exercícios

--- exercises/SenhasAleatorias.js
+++ exercises/SenhasAleatorias.js
@@ -2,18 +2,27 @@
 
 function generatePassword(length, options) {
   const chars = {
-    
+    lower: 'abcdefghijklmnopqrstuvwxyz',
+    upper: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+    number: '0123456789',
+    symbol: '!@#$%^&*()_+~`|}{[]:;?><,./-='
   };
 
   let availableChars = "";
-  if (options.lower) 
-  if (options.upper) 
-  if (options.number) 
-  if (options.symbol);
+  if (options.lower) availableChars += chars.lower;
+  if (options.upper) availableChars += chars.upper;
+  if (options.number) availableChars += chars.number;
+  if (options.symbol) availableChars += chars.symbol;
+  
+  if (!availableChars) {
+    throw new Error('Nenhuma opção válida foi fornecida para a geração da senha.');
+} 
+
 
   let password = "";
   for (let i = 0; i < length; i++) {
-    
+    const randomIndex = Math.floor(Math.random() * availableChars.length);
+        password += availableChars[randomIndex];
   }
 
   return password;
