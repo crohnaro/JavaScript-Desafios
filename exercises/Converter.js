63cee925db11ce81c73542d371c305eb053d298f
crohnaro <vinicioscararine95@gmail.com>
2024-06-05 17:02:40 -0300

    feat: :sparkles: adding the solutions

--- exercises/Converter.js
+++
@@ -1,7 +0,0 @@
-/* Conversor de Unidades de Temperatura: Desenvolva um conversor de unidades de temperatura que transforme valores entre Celsius, Fahrenheit e Kelvin. O usuário deve ser capaz de inserir um valor e selecionar a unidade de origem e destino.*/
-
-function convertTemperature(value, fromUnit, toUnit) {
-    
-}
-
-module.exports = convertTemperature;
